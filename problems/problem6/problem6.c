f0a148a2d7d84c154c693bff3d43f7518ca99457
coffeepenbit <coffeepenbit@gmail.com>
2021-04-28 19:45:07 -0400

    Added problem 6

---
+++ problems/problem6/problem6.c
@@ -0,0 +1,10 @@
+/*
+ * Project Euler
+ * Problem 6
+ */
+
+#include "problem6.h"
+
+int problem6_solution() {
+  return 0;
+}
---
+++ tests/TestProblem6.c
@@ -0,0 +1,12 @@
+#include "unity.h"
+#include "problem6.h"
+
+void test_problem5_solution(void) {
+
+}
+
+int main(void) {
+  UNITY_BEGIN();
+  RUN_TEST(test_problem6_solution);
+  return UNITY_END();
+}
