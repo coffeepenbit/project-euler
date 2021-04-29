2735ae65ca81f5292f769e60465a123c4adf552d
coffeepenbit <coffeepenbit@gmail.com>
2021-04-29 12:21:04 -0400

    Passing test_problem8_array_elem_product

--- include/problem8.h
+++ include/problem8.h
@@ -2,5 +2,7 @@
 #define PROBLEM8_H_INCLUDED
 
 int problem8_solution(void);
+int array_elem_product(int array[], int size_of_array);
+int largest_product_subnumber(char input[], int length_of_input, int subnumber_ndigits);
 
 #endif
--- tests/TestProblem8.c
+++ tests/TestProblem8.c
@@ -5,11 +5,24 @@ void test_problem8_solution(void) {
 
 }
 
+void test_problem8_array_elem_product(void) {
+  TEST_ASSERT_EQUAL_MESSAGE(1, array_elem_product((int[1]){1}, 1), "Input: {1}, 1");
+  TEST_ASSERT_EQUAL_MESSAGE(2, array_elem_product((int[2]){1, 2}, 2), "Input: {1, 2}, 2");
+  TEST_ASSERT_EQUAL_MESSAGE(6, array_elem_product((int[3]){1, 2, 3}, 3), "Input: {1, 2, 3}, 3");
+}
+
+void test_problem8_largest_product_subnumber(void) {
+  TEST_ASSERT_EQUAL_MESSAGE(1, largest_product_subnumber("1", 1, 1), "Input: \"1\", 1, 1");
+}
+
+
 void setUp(void) {}
 void tearDown(void) {}
 
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_problem8_solution);
+  RUN_TEST(test_problem8_array_elem_product);
+  RUN_TEST(test_problem8_largest_product_subnumber);
   return UNITY_END();
 }
