fd4a7a8054846f1abf75ab16d20f73b6d2a3c51a
coffeepenbit <coffeepenbit@gmail.com>
2021-04-30 10:35:54 -0400

    Reference input in header for test cases

--- include/problem8.h
+++ include/problem8.h
@@ -5,4 +5,9 @@ int problem8_solution(void);
 int array_elem_product(int array[], int size_of_array);
 int largest_product_subnumber(char input[], int length_of_input, int subnumber_ndigits);
 
+extern char INPUT[];
+extern int INPUT_LENGTH;
+extern int SUBNUMBER_NDIGITS;
+extern int TEST_CASE_SUBNUMBER_NDIGITS;
+
 #endif
