3a537ac78f380560f892ba56776331061329a0c7
coffeepenbit <coffeepenbit@gmail.com>
2021-04-29 20:25:55 -0400

    Passing tests for largest_product_subnumber

--- tests/TestProblem8.c
+++ tests/TestProblem8.c
@@ -1,8 +1,24 @@
+#include <string.h>
 #include "unity.h"
 #include "problem8.h"
 
 void test_problem8_solution(void) {
+  TEST_ASSERT_EQUAL(1, problem8_solution());
+}
 
+void test_problem8_largest_product_subnumber(void) {
+  TEST_ASSERT_EQUAL_MESSAGE(1, largest_product_subnumber("1", 1, 1), "Input: \"1\", 1, 1");
+  TEST_ASSERT_EQUAL_MESSAGE(2, largest_product_subnumber("2", 1, 1), "Input: \"2\", 1, 1");
+  TEST_ASSERT_EQUAL_MESSAGE(6, largest_product_subnumber("123", 3, 3), "Input: \"123\", 3, 3");
+  TEST_ASSERT_EQUAL_MESSAGE(6, largest_product_subnumber("123", 3, 2), "Input: \"123\", 3, 2");
+  TEST_ASSERT_EQUAL_MESSAGE(3, largest_product_subnumber("123", 3, 1), "Input: \"123\", 3, 2");
+  char multiline_input[] = \
+    "123"
+    "456";
+  TEST_ASSERT_EQUAL_MESSAGE(360, largest_product_subnumber(multiline_input, 6, 4), "Input: \"123\"\n\"456\", 6, 4");
+  strcpy(multiline_input, "654"
+                          "321");
+  TEST_ASSERT_EQUAL_MESSAGE(360, largest_product_subnumber(multiline_input, 6, 4), "Input: \"654\"\n\"321\", 6, 4");
 }
 
 void test_problem8_array_elem_product(void) {
@@ -11,18 +27,13 @@ void test_problem8_array_elem_product(void) {
   TEST_ASSERT_EQUAL_MESSAGE(6, array_elem_product((int[3]){1, 2, 3}, 3), "Input: {1, 2, 3}, 3");
 }
 
-void test_problem8_largest_product_subnumber(void) {
-  TEST_ASSERT_EQUAL_MESSAGE(1, largest_product_subnumber("1", 1, 1), "Input: \"1\", 1, 1");
-}
-
-
 void setUp(void) {}
 void tearDown(void) {}
 
 int main(void) {
   UNITY_BEGIN();
   RUN_TEST(test_problem8_solution);
-  RUN_TEST(test_problem8_array_elem_product);
   RUN_TEST(test_problem8_largest_product_subnumber);
+  RUN_TEST(test_problem8_array_elem_product);
   return UNITY_END();
 }
